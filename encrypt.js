75fd21836c22c36baba01de16f4e6f4488cc6ac3
AmmaarShaikh <99209085+AmmaarShaikh@users.noreply.github.com>
2023-06-02 13:57:08 -0400

    Add files via upload

--- encrypt.js
+++ encrypt.js
@@ -1,9 +1,11 @@
 function caesarCipher(phrase, shifter){
-    letters = phrase.charCodeAt();
-    newPhrase = ""
-    for (letter in letters){
-        console.log(letter)
-        newPhrase += String.fromCharCode(letter)
+    letters = phrase.split('');
+    newPhrase = "Your new phrase is :"
+    for (letter of letters){
+        newLetter = letter.charCodeAt(0)
+        console.log(newLetter)
+        newLetter += shifter
+        newPhrase += String.fromCharCode(newLetter)
     }
-        
+    document.getElementById("output").innerHTML = newPhrase;
 }
